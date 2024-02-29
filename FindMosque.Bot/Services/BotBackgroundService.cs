1c9f006e3e107155191c1502a2420c8451ac0652
Jurayevkh <jurayevkh@gmail.com>
2024-03-01 01:07:45 +0500

    BackgroundService

---
+++ FindMosque.Bot/Services/BotBackgroundService.cs
@@ -0,0 +1,31 @@
+﻿
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+
+namespace FindMosque.Bot.Services;
+
+public class BotBackgroundService : BackgroundService
+{
+    private readonly ILogger<BackgroundService> _logger;
+    private readonly TelegramBotClient _client;
+    private readonly IUpdateHandler _updateHandler;
+
+    public BotBackgroundService(ILogger<BackgroundService> logger, TelegramBotClient client, IUpdateHandler updateHandler)
+    {
+        _logger = logger;
+        _client = client;
+        _updateHandler = updateHandler;
+    }
+
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        var bot = await _client.GetMeAsync(stoppingToken);
+        _logger.LogInformation($"Bot succesfully started : {bot.Username}");
+
+        _client.StartReceiving(
+            _updateHandler.HandleUpdateAsync,
+            _updateHandler.HandlePollingErrorAsync,
+            new ReceiverOptions() {ThrowPendingUpdates=true}, stoppingToken);
+    }
+}
