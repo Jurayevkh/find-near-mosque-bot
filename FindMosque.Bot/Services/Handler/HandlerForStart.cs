31f59bf7a0397ce24d734e93c480742c21ee6775
Jurayevkh <jurayevkh@gmail.com>
2024-03-01 01:07:34 +0500

    Created Handler For Start

---
+++ FindMosque.Bot/Services/Handler/HandlerForStart.cs
@@ -0,0 +1,21 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace FindMosque.Bot;
+
+public partial class BotUpdateHandler
+{
+    private async Task HandlerForStart(ITelegramBotClient botClient,Message message, CancellationToken cancellationToken)
+    {
+        var user = message.From;
+        await botClient.SendTextMessageAsync
+            (
+            chatId: user.Id,
+            text: $"start received from {user.FirstName}",
+            replyToMessageId: message.MessageId,
+            cancellationToken: cancellationToken
+            );
+
+    }
+}
+
