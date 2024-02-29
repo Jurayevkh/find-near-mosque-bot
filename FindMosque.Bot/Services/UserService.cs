258014432f6fad12264a41787b59a9ca7f2d9dce
Jurayevkh <jurayevkh@gmail.com>
2024-03-01 01:07:21 +0500

    Just Created UserService

---
+++ FindMosque.Bot/Services/UserService.cs
@@ -0,0 +1,12 @@
+﻿using FindMosque.Bot.Entity;
+
+namespace FindMosque.Bot.Services;
+
+public class UserService
+{
+    public async Task<User> GetUserAsync(long accoundId)
+    {
+        return new User() { LanguageCode="uz-Uz"};
+    }
+}
+
