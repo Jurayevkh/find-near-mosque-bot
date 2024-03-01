82165545104e05518224e3d0212151aa35ca8547
Jurayevkh <jurayevkh@gmail.com>
2024-03-02 00:54:58 +0500

    Handler for start

--- FindMosque.Bot/Services/Handler/HandlerForStart.cs
+++ FindMosque.Bot/Services/Handler/HandlerForStart.cs
@@ -1,5 +1,6 @@
 ﻿using Telegram.Bot;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
 
 namespace FindMosque.Bot;
 
@@ -7,14 +8,38 @@ public partial class BotUpdateHandler
 {
     private async Task HandlerForStart(ITelegramBotClient botClient,Message message, CancellationToken cancellationToken)
     {
+        InlineKeyboardMarkup inlineKeyboard = new(new[]
+        {
+            // first row
+            new []
+            {
+                InlineKeyboardButton.WithCallbackData(text: "1.1", callbackData: "11"),
+                InlineKeyboardButton.WithCallbackData(text: "1.2", callbackData: "12"),
+            },
+            // second row
+            new []
+            {
+                InlineKeyboardButton.WithCallbackData(text: "2.1", callbackData: "21"),
+                InlineKeyboardButton.WithCallbackData(text: "2.2", callbackData: "22"),
+            },
+        });
+        //ReplyKeyboardMarkup replyKeyboardMarkup = new(new[]
+        //    {
+        //         new KeyboardButton[] { "O'zbek", "Rus" },
+        //    })
+        //{
+        //    ResizeKeyboard = true
+        //};
+
         var user = message.From;
         await botClient.SendTextMessageAsync
             (
             chatId: user.Id,
             text: $"start received from {user.FirstName}",
             replyToMessageId: message.MessageId,
+            replyMarkup:inlineKeyboard,
             cancellationToken: cancellationToken
-            );
+            ) ;
 
     }
 }
