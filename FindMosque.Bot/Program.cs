2ede568c1c3b2703bd50ae809147c4c44835dd44
Jurayevkh <jurayevkh@gmail.com>
2024-03-02 00:54:18 +0500

    Set

--- FindMosque.Bot/Program.cs
+++ FindMosque.Bot/Program.cs
@@ -1,11 +1,15 @@
+using FindMosque.Bot;
+using FindMosque.Bot.Services;
 using Telegram.Bot;
+using Telegram.Bot.Polling;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var token=builder.Configuration.GetValue("BotToken",string.Empty);
+var token = builder.Configuration.GetValue("BotToken", string.Empty);
 
-builder.Services.AddSingleton(new TelegramBotClient(token));
-// builder.Services.AddHostedService<StartingBotBackgroundService>();
+builder.Services.AddSingleton(p => new TelegramBotClient(token!));
+builder.Services.AddSingleton<IUpdateHandler, BotUpdateHandler>();
+builder.Services.AddHostedService<BotBackgroundService>();
 
 
 var app = builder.Build();
