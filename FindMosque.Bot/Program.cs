d007b7a233ab16a1aa5f443c41f5fb198784bdbb
Jurayevkh <jurayevkh@gmail.com>
2024-03-03 01:00:36 +0500

    Set

--- FindMosque.Bot/Program.cs
+++ FindMosque.Bot/Program.cs
@@ -1,9 +1,23 @@
 using FindMosque.Bot;
+using FindMosque.Bot.Data;
 using FindMosque.Bot.Services;
+using Microsoft.EntityFrameworkCore;
 using Telegram.Bot;
 using Telegram.Bot.Polling;
 
+//var builder = WebApplication.CreateBuilder(args);
+
+//var token = builder.Configuration.GetValue("BotToken", string.Empty);
+
+//builder.Services.AddSingleton(p => new TelegramBotClient(token!));
+//builder.Services.AddSingleton<IUpdateHandler, BotUpdateHandler>();
+//builder.Services.AddHostedService<BotBackgroundService>();
+
+
+//var app = builder.Build();
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<BotDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var token = builder.Configuration.GetValue("BotToken", string.Empty);
 
@@ -11,7 +25,19 @@ builder.Services.AddSingleton(p => new TelegramBotClient(token!));
 builder.Services.AddSingleton<IUpdateHandler, BotUpdateHandler>();
 builder.Services.AddHostedService<BotBackgroundService>();
 
+builder.Services.AddScoped<UserService>();
+
+builder.Services.AddLocalization();
 
 var app = builder.Build();
+
+var supportedCultures = new[] { "uz-Uz", "en-US", "ru-Ru" };
+var localizationOptions = new RequestLocalizationOptions()
+    .SetDefaultCulture(supportedCultures[1])
+    .AddSupportedCultures(supportedCultures)
+    .AddSupportedUICultures(supportedCultures);
+
+app.UseRequestLocalization(localizationOptions);
+
 
 app.Run();
